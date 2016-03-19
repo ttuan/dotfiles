48561d016886ace683529e583147e2966cc844a0
ttuan <tuantv.nhnd@gmail.com>
2016-03-19 12:34:46 +0700

    Add Ack, fish, vimperator and i3 config file

---
+++ config.fish
@@ -0,0 +1,24 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH /home/whoami/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /home/whoami/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+# Config rvm for ruby on rails
+set fish_plugins git rails
+rvm default
+eval (python -m virtualfish)
+
+# Turn off fish greeting
+set fish_greeting ""
+
+# Set Alias
+alias lsl "ls -l"
+alias bi "bundle install"
+alias go "gnome-open"
+alias update "sudo apt-get update"
+alias install "sudo apt-get install"
+alias addrp "sudo add-apt-repository"
