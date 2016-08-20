faca7c674df85d17329e1ca8538e30c0ec758399
ttuan <tuantv.nhnd@gmail.com>
2016-08-20 08:52:58 +0700

    Split alias into other file

---
+++ zsh/alias.zsh
@@ -0,0 +1,80 @@
+DOTFILE=~/Dropbox/Projects/dotfile
+
+# Alias for Git
+alias gpf="git pull framgia master"
+alias gd="git diff @~..@"
+alias grs="git reset HEAD~1"
+alias gst="git status -s"
+alias gsta="git add .; git stash"
+
+gf() {
+  git add .;
+  git commit -m $1;
+  branch_name=$(git symbolic-ref --short -q HEAD);
+  git push origin $branch_name;
+  repo_url=$(git config --get remote.origin.url)
+  firefox $repo_url;
+}
+
+gff() {
+  git add .;
+  git commit --amend;
+  branch_name=$(git symbolic-ref --short -q HEAD);
+  git push origin $branch_name -f;
+}
+
+grb() {
+  curr_branch=$(git symbolic-ref --short -q HEAD);
+  git checkout $1;
+  git pull framgia $1;
+  git checkout $curr_branch;
+  git rebase $1;
+}
+
+
+#\\ System
+alias go="gnome-open"
+alias bi="bundle install"
+alias tmx="tmuxinator start project $1"
+alias serve="python -m SimpleHTTPServer 8000"
+
+alias !="sudo"
+alias rr="rm -rf"
+alias q="exit"
+alias c="clear"
+alias s="source ~/.zshrc"
+
+alias h="history | grep"
+alias ps="ps auxf | grep"
+
+alias kill="sudo killall -9"
+
+# ls
+alias ll="ls -lah"
+alias la="ls -A"
+alias l="ls"
+
+# File system tree
+alias .='pwd'
+alias ..='cd ..'
+alias ...='cd ../..'
+
+alias dot="cd $DOTFILE"
+
+mkcd() {
+  mkdir "$1"
+  cd "$1"
+}
+search() { find . -iname "*$@*" | less; }
+
+#\\ Application
+alias update="sudo apt-get update"
+alias install="sudo apt-get install"
+alias add="sudo add-apt-repository"
+
+# Extend
+# Weather
+weather() {
+  local CITY=${1:-Hanoi}
+  curl -4 "wttr.in/$CITY"
+}
