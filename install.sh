f0e1c48d7ce2d221f4e53d4a5294460cc7d3e542
ttuan <tuantv.nhnd@gmail.com>
2018-02-05 10:40:28 +0700

    Add config for grv

--- install.sh
+++ install.sh
@@ -83,7 +83,7 @@ function install_ruby_on_rails {
 
   sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  curl -sSL https://get.rvm.io -ip4 | bash -s stable
+  curl -sSL https://get.rvm.io | bash -s stable
   source ~/.rvm/scripts/rvm
   rvm install 2.3.1
   rvm use 2.3.1 --default
@@ -111,6 +111,11 @@ function programs {
   sudo apt-get install -y xpad
   sudo apt-get install -y nautilus-dropbox
   sudo apt-get install -y ncdu
+
+  # GRV for viewing git project
+  wget -O grv https://github.com/rgburke/grv/releases/download/v0.1.1/grv_v0.1.1_linux64
+  chmod +x ./grv
+
   # Alfred like for Ubuntu
   sudo add-apt-repository ppa:nilarimogard/webupd8
   sudo apt-get install -y albert
