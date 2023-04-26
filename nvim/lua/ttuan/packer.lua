6562ce558a42f23946b18da3d3c40bfe593427b3
ttuan <tuantv.nhnd@gmail.com>
2023-04-26 11:00:08 +0700

    Nvim RC from Scratch

---
+++ nvim/lua/ttuan/packer.lua
@@ -0,0 +1,51 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Find, Filter, Preview, Pick
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Colorscheme
+  use { "ellisonleao/gruvbox.nvim" }
+
+  -- Syntax highlight
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+  -- Navigating files set and terminal
+  use('ThePrimeagen/harpoon')
+
+  -- Undotree
+  use('mbbill/undotree')
+
+  use('tpope/vim-fugitive')
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v2.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {                                      -- Optional
+		  'williamboman/mason.nvim',
+		  run = function()
+			  pcall(vim.cmd, 'MasonUpdate')
+		  end,
+		  },
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},     -- Required
+		  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+		  {'L3MON4D3/LuaSnip'},     -- Required
+          }
+  }
+end)
---
+++ nvim/lua/ttuan/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
