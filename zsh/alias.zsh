e9ed245c5cc1609a3ad42e3615a145b85c2c5891
ttuan <tuantv.nhnd@gmail.com>
2023-05-04 11:35:51 +0700

    feat(zsh): Use exa command line tool for ls alternative

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -35,7 +35,31 @@ alias ...='cd ../..'
 alias j='z'
 
 # ls
-alias ll="ls -lah"
+export EXA_COLORS="\
+da=38;5;245:\
+di=38;5;14:\
+sn=38;5;28:\
+sb=38;5;28:\
+uu=38;5;40:\
+un=38;5;160:\
+gu=38;5;40:\
+gn=38:5:160:\
+bl=38;5;220:\
+ur=37:\
+uw=37:\
+ux=37:\
+ue=37:\
+gr=37:\
+gw=37:\
+gx=37:\
+tr=37:\
+tw=37:\
+tx=37:\
+su=37:\
+sf=37:\
+xa=37"
+alias ll="exa -l -g --icons --sort=.name  --group-directories-first"
+alias lla="ll -a"
 alias la="ls -A"
 alias l="ls"
 
