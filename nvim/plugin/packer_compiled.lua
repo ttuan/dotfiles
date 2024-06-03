43cf48f88b0e2c7da9a0cc72dfcca6848f09b455
ttuan <tuantv.nhnd@gmail.com>
2024-06-03 10:06:58 +0700

    nvim - Use telescope buffers instead of harpoon

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -119,11 +119,6 @@ _G.packer_plugins = {
     path = "/Users/tran.van.tuan/.local/share/nvim/site/pack/packer/start/gruvbox",
     url = "https://github.com/morhetz/gruvbox"
   },
-  harpoon = {
-    loaded = true,
-    path = "/Users/tran.van.tuan/.local/share/nvim/site/pack/packer/start/harpoon",
-    url = "https://github.com/ThePrimeagen/harpoon"
-  },
   ["hop.nvim"] = {
     loaded = true,
     path = "/Users/tran.van.tuan/.local/share/nvim/site/pack/packer/start/hop.nvim",
