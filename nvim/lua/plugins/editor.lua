80100da9257dc1fe973d9b3afdd74cb702128082
ttuan <tuantv.nhnd@gmail.com>
2026-08-08 11:20:16 +0700

    chore(nvim): Remove Copilot integration

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -205,34 +205,6 @@ return {
     },
   },
 
-  -- Override nvim-cmp keymaps
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          -- attach cmp source whenever copilot attaches
-          -- fixes lazy-loading issues with the copilot cmp source
-          LazyVim.lsp.on_attach(function(client)
-            copilot_cmp._on_insert_enter({})
-          end, "copilot")
-        end,
-      },
-    },
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
-
   -- Telescope
   -- Reference: https://github.com/craftzdog/dotfiles-public/blob/master/.config/nvim/lua/plugins/editor.lua
   {
